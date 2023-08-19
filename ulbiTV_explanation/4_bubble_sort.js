5e3bdfde4532852979acda325286765657728286
Nazar Koval <n.kovaldev@gmail.com>
2023-08-19 07:28:35 +0300

    add BubbleSort algorithm

--- ulbiTV_explanation/4_bubble_sort.js
+++ ulbiTV_explanation/4_bubble_sort.js
@@ -1,5 +1,4 @@
 const arr = [0, 3, 2, 5, 6, 8, 23, 9, 4, 2, 1, 2, 9, 6, 4, 1, 7, -1, -5, 23, 6, 2, 35, 6, 3, 32, 9, 4, 2, 1, 2, 9, 6, 4, 1, 7, -1, -5, 23, 9, 4, 2, 1, 2, 9, 6, 4, 1, 7, -1, -5, 23]
-let count = 0
 
 function bubbleSort(array) {
 	for (let i = 0; i < array.length; i++) {
@@ -8,7 +7,6 @@ function bubbleSort(array) {
 				let tmp = array[j]
 				array[j] = array[j + 1]
 				array[j + 1] = tmp
-            count++
 			}
 		}
 	}
@@ -16,4 +14,3 @@ function bubbleSort(array) {
 }
 
 console.log(bubbleSort(arr)) // O(n*n)
-console.log(count) // O(n*n)
